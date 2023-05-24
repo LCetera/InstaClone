dff4d4e763bb6e8f5715ad18d608907a6c675b8a
LCetera <l.cetera@gmail.com>
2023-05-24 22:00:29 +0200

    Search image height changed for ratio fix

--- src/components/ImageComponent.js
+++ src/components/ImageComponent.js
@@ -1,12 +1,15 @@
 import { View, Image, Dimensions } from 'react-native';
 
+const dimension = Dimensions.get('window');
+const imageWidth = dimension.width / 3;
+
 export default ImageComponent = ({ imageURI }) => (
   <View>
     <Image
       source={{
         uri: imageURI,
       }}
-      style={{ width: Dimensions.get('window').width / 3, height: 150 }}
+      style={{ width: imageWidth, height: imageWidth }}
     />
   </View>
 );
